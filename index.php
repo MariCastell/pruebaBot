093d62eb7f71ce0958a799783a3a02310fb9c527
MariCastell <50238809+MariCastell@users.noreply.github.com>
2019-06-29 16:26:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>PRUEBA BOT RSS</title>
 </head>
 <body>
-	<p> HOLA </p>
+	<?php
+		$botToken = "883869979:AAHz87uoLDAngUOo5FxvhzM9PuIjQDk6J3E";
+		$website = "https://api.telegram.org/bot".$botToken;
+	
+		$update = file_get_contents('php://imput');
+		$update = json_decode($update, TRUE);
+	
+		$chatId = $update["message"]["chat"]["id"];
+		$chatType = $update["message"]["chat"]["type"];
+	
+		$message = $update["message"]["text"];
+	
+		switch($message){
+			case '/ayuda':
+				$response = "Vale";
+				sendMessage($chatId, $response);
+				break;
+		}
+		
+		function sendMessage($chatId, $response){
+			$url = 	$GLOBALS[website] '/sendMessage?chat_id='.$chatId."&parse_mode=HTML&text=".urlencode($response);
+			file_get_contents($url);
+		}
+		
+	?>
 </body>
 </html>
